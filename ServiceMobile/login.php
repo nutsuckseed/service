46a544b240b9d05da6dc5a093e8c51b938283fb1
kuhasun <kuhasun@live.com>
2018-03-08 12:56:01 +0700

    asdfghjkl;

--- ServiceMobile/login.php
+++ ServiceMobile/login.php
@@ -10,17 +10,10 @@ $app->POST('/login', function($request , $response , $args)  {
 	 $json = $request->getBody(); //POST
     $jsonArr = json_decode($json, true, 512, JSON_UNESCAPED_UNICODE); //
     $username = isset($jsonArr['username'])?$jsonArr['username']:"";
-    $password = isset($jsonArr['password'])?mysqli_real_escape_string($conn,md5($jsonArr['password'])):"";
+    $password = isset($jsonArr['password'])?mysqli_real_escape_string($conn,md5($jsonArr['password'])):$jsonArr['password'];
 
                 $sql = "SELECT * FROM tbl_users WHERE (username='$username' or email='$username') and password='$password' ";
                 $result = $conn->query($sql);
-
-    // if(!empty($result))
-    //     {
-    //         $user_id=$result->id;
-    //         // var_dump("user_id",$user_id);
-    //         $userData->token = apiToken($user_id);
-    //     }
 
                $arr = array();
                 
